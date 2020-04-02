52a5f1f4400a8946eabfe5cfaada9aa519f85e75
GeryMolina <g.molina.ojeda@gmail.com>
2020-04-02 15:13:18 -0300

    creacion componente Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Login from './components/auth/Login';
-import SignIn from './components/auth/SignIn';
+import Home from './views/Home'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
-      <SignIn/>
+      <Home />
     </div>
   );
 }
